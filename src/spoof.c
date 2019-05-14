36378405018b289ea4c104b7673eee211553d772
arthur <arthur.teisseire@epitech.eu>
2019-05-14 17:51:49 +0200

    Refactor printBroadcast

--- src/spoof.c
+++ src/spoof.c
@@ -12,23 +12,29 @@
 #include <netinet/ether.h>
 #include "arpspoof.h"
 
+void print_packet(const unsigned char *packet, size_t size)
+{
+    printf("%02x", packet[0]);
+    for (size_t i = 1; i < size; ++i)
+        printf(" %02x", packet[i]);
+    printf("\n");
+}
+
 int print_spoofed(arp_t *arp)
 {
-    (void)arp;
-    printf("printing spoof\n");
+    arp_packet_t packet;
+
+    init_broadcast(&packet, arp);
+    print_packet((unsigned char *)&packet, sizeof(packet));
     return 0;
 }
 
 int print_broadcast(arp_t *arp)
 {
     arp_packet_t packet;
-    unsigned char *byte_array = (unsigned char *)&packet;
 
     init_broadcast(&packet, arp);
-    printf("%02x", byte_array[0]);
-    for (size_t i = 1; i < sizeof(packet); ++i)
-        printf(" %02x", byte_array[i]);
-    printf("\n");
+    print_packet((unsigned char *)&packet, sizeof(packet));
     return 0;
 }
 
